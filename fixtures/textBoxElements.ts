75a9cec866eb34d741443532673f55fd8e7a98c7
Joao Romao <tupi_1800@hotmail.com>
2025-09-28 18:29:20 -0300

    Add more tests to text box page

--- fixtures/textBoxElements.ts
+++ fixtures/textBoxElements.ts
@@ -13,6 +13,7 @@ export const LOCATORS = {
 export const TEXT = {
     FULL_NAME: "John Doe",
     EMAIL: "email@email.com",
+    INCOMPLETE_EMAIL: "email",
     CURRENT_ADDRESS: "151  CALLE LUIS MUNOZ RIVERA  GUAYANILLA PR 00656-1753 USA",
     PERMANENT_ADDRESS: "9180  HC 4  CANOVANAS PR 00729-9871 USA",
     OUTPUT_NAME: "Name:John Doe",
