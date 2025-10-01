c11857d4c4b0a381e968dd45c3578756fb7dea83
João Romão <tupi_1800@hotmail.com>
2025-09-30 22:00:49 -0300

    correcting checkbox test

--- tests/checkbox.spec.ts
+++ tests/checkbox.spec.ts
@@ -7,15 +7,37 @@ test.beforeEach(async ({ page }) => {
 test.describe("Element Interaction Tests - check box", () => {
   test("should expand all folders and verify all items are checked", async ({ page }) => {
     await page.click('button[title="Expand all"]');
+    await page.click('//span[text()="Home"]');
+
     const checkboxes = page.locator('.rct-checkbox');
-    const checkboxCount = await checkboxes.count();
-
-    for (let i = 0; i < checkboxCount; i++) {
-      await checkboxes.nth(i).click();
-    }
-
     for (const checkbox of await checkboxes.all()) {
       await expect(checkbox).toBeChecked();
     }
+
+    const expectedItems = [
+      "home",
+      "desktop",
+      "notes",
+      "commands",
+      "documents",
+      "workspace",
+      "react",
+      "angular",
+      "veu",
+      "office",
+      "public",
+      "private",
+      "classified",
+      "general",
+      "downloads",
+      "wordFile",
+      "excelFile",
+    ];
+
+    const resultItemsLocator = page.locator("#result .text-success");
+
+    // Verifica se todos os textos esperados estão visíveis de uma só vez.
+    // A ordem não importa para toHaveText quando o argumento é um array.
+    await expect(resultItemsLocator).toHaveText(expectedItems);
   });
 });
